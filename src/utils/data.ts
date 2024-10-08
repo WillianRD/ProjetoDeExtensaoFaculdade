b4b604bd6416e29beacdfa214765e6499b64fdb4
WillianRD <willianrodriguesdos1@gmail.com>
2024-10-07 21:47:02 -0300

    Nova Atualização

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,37 +1,37 @@
 const services = [
     {
-        title: "Consultórias",
-        description: "Com um atendimento atencioso e personalizado, buscamos não apenas cuidar da sua saúde bucal, mas também criar um ambiente onde você se sinta confortável e seguro.",
+        title: "Consultas Odontológicas",
+        description: "As consultas odontológicas são fundamentais para avaliar e manter a saúde bucal, garantindo um sorriso saudável e prevenindo problemas futuros.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Endereço",
-        description: "Endereço: Rua. Marechal Deodoro, N° 1564  2º Andar - Sala 09 - Centro, São Bernardo do Campo - SP, 09710-010",
+        title: "Limpeza Dental",
+        description: "A limpeza dental remove placas e tártaro, prevenindo cáries e doenças gengivais, garantindo um sorriso mais saudável.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Email",
-        description: "DrRaquel.gmail.com",
+        title: "Restaurações",
+        description: "As restaurações odontológicas são procedimentos que visam recuperar dentes danificados por cáries ou traumas, restaurando sua forma, função e estética.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Horário de Atendimento",
-        description: "Segunda/Terça/Quarta/Quinta/Sexta das 09:00 - 13:00 | 14:00 - 17:00 & Último sábado do mês das 09:00 as 15:00",
+        title: "Ortodontia",
+        description: "A ortodontia é a especialidade que corrige problemas de alinhamento dental e irregularidades na arcada dentária, utilizando aparelhos ortodônticos para garantir um sorriso saudável e harmônico.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Serviços Oferecidos",
-        description: "Orçamento totalmente grátis",
+        title: "Tratamento de Canal",
+        description: "O tratamento de canal é um procedimento que remove a infecção da polpa dental, aliviando a dor e salvando o dente. Ele é essencial para preservar a saúde bucal e evitar extrações.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Redes Sociais",
-        description: "Instagram: draraquelhattori ",
+        title: "Odontopediatria",
+        description: "A odontopediatria é a especialidade que cuida da saúde bucal de crianças e adolescentes, focando na prevenção e tratamento de problemas dentários, além de promover hábitos saudáveis desde cedo.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
